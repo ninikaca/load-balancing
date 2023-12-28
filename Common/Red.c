f6c0c1611b0a88f9fa43b048637c3ff6b678b4e0
Katarina Marinković <ninikaca@gmail.com>
2023-12-28 17:41:41 +0100

    Create queue structure

---
+++ Common/Red.c
@@ -0,0 +1,70 @@
+﻿// Red.c
+
+#include "Red.h"
+
+// Funkcija za inicijalizaciju reda
+void inicijalizujRed(Red* red) {
+    red->pocetak = red->kraj = NULL;
+    InitializeCriticalSection(&red->kriticna_sekcija);
+}
+
+// Funkcija za dodavanje elementa u red
+void dodajURed(Red* red, int vrednost) {
+    RedCvor* noviCvor = (RedCvor*)malloc(sizeof(RedCvor));
+    if (noviCvor == NULL) {
+        // Obrada greške ako alokacija memorije nije uspela
+        return;
+    }
+    noviCvor->podatak = vrednost;
+    noviCvor->sledeci = NULL;
+
+    EnterCriticalSection(&red->kriticna_sekcija);
+
+    if (red->kraj == NULL) {
+        red->pocetak = red->kraj = noviCvor;
+    }
+    else {
+        red->kraj->sledeci = noviCvor;
+        red->kraj = noviCvor;
+    }
+
+    LeaveCriticalSection(&red->kriticna_sekcija);
+}
+
+// Funkcija za uklanjanje elementa iz reda
+int ukloniIzReda(Red* red) {
+    EnterCriticalSection(&red->kriticna_sekcija);
+
+    if (jePrazan(red)) {
+        LeaveCriticalSection(&red->kriticna_sekcija);
+        return INT_MIN; // Vraća minimalnu vrednost za int kao indikator greške ili praznog reda
+    }
+
+    RedCvor* temp = red->pocetak;
+    int podatak = temp->podatak;
+
+    red->pocetak = red->pocetak->sledeci;
+
+    if (red->pocetak == NULL) {
+        red->kraj = NULL;
+    }
+
+    free(temp);
+
+    LeaveCriticalSection(&red->kriticna_sekcija);
+
+    return podatak;
+}
+
+// Funkcija za proveru da li je red prazan
+bool jePrazan(Red* red) {
+    return red->pocetak == NULL;
+}
+
+// Funkcija za uništavanje reda
+void unistiRed(Red* red) {
+    while (!jePrazan(red)) {
+        ukloniIzReda(red);
+    }
+    DeleteCriticalSection(&red->kriticna_sekcija);
+}
---
+++ Common/Red.h
@@ -0,0 +1,25 @@
+﻿#ifndef RED_H
+#define RED_H
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <WinSock2.h>
+#include "RedCvor.h"
+
+/*
+ * Struktura Red:
+ * --------------
+ * Struktura koja predstavlja red sa podrškom za sinhronizaciju pristupa (thread-safe queue).
+ *
+ * Clanovi strukture:
+ *  - pocetak: Pokazivac na prvi element u redu.
+ *  - kraj: Pokazivac na poslednji element u redu.
+ *  - kriticna_sekcija: Kritična sekcija za sinhronizaciju pristupa redu.
+ */
+typedef struct {
+    RedCvor* pocetak;
+    RedCvor* kraj;
+    CRITICAL_SECTION kriticna_sekcija; // Kritična sekcija za sinhronizaciju pristupa redu
+} Red;
+
+#endif /* RED_H */
