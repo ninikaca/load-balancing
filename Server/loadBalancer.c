0ed96ab4247e3ad30a2d34c537f8206f3afc9a04
Katarina Marinković <ninikaca@gmail.com>
2024-01-02 17:46:43 +0100

    Load Balancer: Implement balancing algorithm

--- Server/loadBalancer.c
+++ Server/loadBalancer.c
@@ -26,6 +26,11 @@ int IzvrsiSkladistenje(LoadBalancerParametri* parametri, int podatak) {
     // Slanje podatka na najmanje "opterecenog" workera
     WorkerParametri wp = { brojSkladistenihPodatakaWorker, red, podatak };
     workers[minIndex] = (HANDLE)_beginthreadex(NULL, 0, &Skladistenje, (void *)&wp, 0, NULL);
+    
+    if (workers[minIndex] != NULL) {
+        WaitForSingleObject(workers[minIndex], 1000);
+        CloseHandle(workers[minIndex]);
+    }
 
     printf("[Skladiste ID = %d]: Podatak skladisten u skladiste.\n", minIndex);
 
